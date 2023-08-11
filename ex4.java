42e6e2e487180933a515b6e14c9f986308216c15
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2023-08-11 23:08:22 +0530

    Create ex4.java

---
+++ ex4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        String reversed = reverseString(input);
+        
+        System.out.println("Reversed string: " + reversed);
+        
+        scanner.close();
+    }
+    
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
